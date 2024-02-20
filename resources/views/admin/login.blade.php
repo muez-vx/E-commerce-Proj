7b76c0c021a2b8d2a1d3872be88472c279b63141
muez_vx <bc180402947@vu.edu.pk>
2024-02-20 23:31:39 +0500

    Auth#8

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -24,7 +24,7 @@
 					<form action="{{ route ('admin.authenticate')}}" method="post">
 						@csrf
 				  		<div class="input-group mb-3">
-							<input type="email" name="email" id="email" class="form-control " placeholder="Email">
+							<input type="email" name="email" id="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email">
 							<div class="input-group-append">
 					  			<div class="input-group-text">
 									<span class="fas fa-envelope"></span>
@@ -37,14 +37,14 @@
 							
 				  		</div>
 				  		<div class="input-group mb-3">
-							<input type="password"  name="password" id="password" class="form-control" placeholder="Password">
+							<input type="password"  name="password" id="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password">
 							<div class="input-group-append">
 					  			<div class="input-group-text">
 									<span class="fas fa-lock"></span>
 					  			</div>
 							</div>
 							@error('password')
-							<p>{{ $message }}</p>
+							<p class="invalid-feedback" > {{ $message }} </p>
 							@enderror
 				  		</div>
 				  		<div class="row">
