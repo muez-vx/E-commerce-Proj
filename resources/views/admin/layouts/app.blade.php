7133c421f87f52e9f49c70fcd6aea8c4a4aa4ac3
muez_vx <bc180402947@vu.edu.pk>
2024-02-28 23:30:15 +0500

    Admin Panel Conf Com

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -40,8 +40,8 @@
 							<img src="{{ asset('admin_assets/img/avatar5.png') }}" class='img-circle elevation-2' width="40" height="40" alt="">
 						</a>
 						<div class="dropdown-menu dropdown-menu-lg dropdown-menu-right p-3">
-							<h4 class="h4 mb-0"><strong>Mohit Singh</strong></h4>
-							<div class="mb-3">example@example.com</div>
+							<h4 class="h4 mb-0"><strong>{{Auth::guard('admin')->user()->name }}</strong></h4>
+							<div class="mb-3">{{Auth::guard('admin')->user()->email }}</div>
 							<div class="dropdown-divider"></div>
 							<a href="#" class="dropdown-item">
 								<i class="fas fa-user-cog mr-2"></i> Settings								
@@ -51,7 +51,7 @@
 								<i class="fas fa-lock mr-2"></i> Change Password
 							</a>
 							<div class="dropdown-divider"></div>
-							<a href="#" class="dropdown-item text-danger">
+							<a href="{{route('admin.logout')}}" class="dropdown-item text-danger">
 								<i class="fas fa-sign-out-alt mr-2"></i> Logout							
 							</a>							
 						</div>
@@ -60,89 +60,7 @@
 			</nav>
 			<!-- /.navbar -->
 			<!-- Main Sidebar Container -->
-			<aside class="main-sidebar sidebar-dark-primary elevation-4">
-				<!-- Brand Logo -->
-				<a href="#" class="brand-link">
-					<img src="{{ asset('admin_assets/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-					<span class="brand-text font-weight-light">LARAVEL SHOP</span>
-				</a>
-				<!-- Sidebar -->
-				<div class="sidebar">
-					<!-- Sidebar user (optional) -->
-					<nav class="mt-2">
-						<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-							<!-- Add icons to the links using the .nav-icon class
-								with font-awesome or any other icon font library -->
-							<li class="nav-item">
-								<a href="dashboard.html" class="nav-link">
-									<i class="nav-icon fas fa-tachometer-alt"></i>
-									<p>Dashboard</p>
-								</a>																
-							</li>
-							<li class="nav-item">
-								<a href="categories.html" class="nav-link">
-									<i class="nav-icon fas fa-file-alt"></i>
-									<p>Category</p>
-								</a>
-							</li>
-							<li class="nav-item">
-								<a href="subcategory.html" class="nav-link">
-									<i class="nav-icon fas fa-file-alt"></i>
-									<p>Sub Category</p>
-								</a>
-							</li>
-							<li class="nav-item">
-								<a href="brands.html" class="nav-link">
-									<svg class="h-6 nav-icon w-6 shrink-0" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-										<path stroke-linecap="round" stroke-linejoin="round" d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
-									  </svg>
-									<p>Brands</p>
-								</a>
-							</li>
-							<li class="nav-item">
-								<a href="products.html" class="nav-link">
-									<i class="nav-icon fas fa-tag"></i>
-									<p>Products</p>
-								</a>
-							</li>
-							
-							<li class="nav-item">
-								<a href="#" class="nav-link">
-									<!-- <i class="nav-icon fas fa-tag"></i> -->
-									<i class="fas fa-truck nav-icon"></i>
-									<p>Shipping</p>
-								</a>
-							</li>							
-							<li class="nav-item">
-								<a href="orders.html" class="nav-link">
-									<i class="nav-icon fas fa-shopping-bag"></i>
-									<p>Orders</p>
-								</a>
-							</li>
-							<li class="nav-item">
-								<a href="discount.html" class="nav-link">
-									<i class="nav-icon  fa fa-percent" aria-hidden="true"></i>
-									<p>Discount</p>
-								</a>
-							</li>
-							<li class="nav-item">
-								<a href="users.html" class="nav-link">
-									<i class="nav-icon  fas fa-users"></i>
-									<p>Users</p>
-								</a>
-							</li>
-							<li class="nav-item">
-								<a href="pages.html" class="nav-link">
-									<i class="nav-icon  far fa-file-alt"></i>
-									<p>Pages</p>
-								</a>
-							</li>							
-						</ul>
-					</nav>
-					<!-- /.sidebar-menu -->
-				</div>
-				<!-- /.sidebar -->
-         	</aside>
+			@include('admin.layouts.sidebar')
 			<!-- Content Wrapper. Contains page content -->
 			<div class="content-wrapper">
 				
