67072c6b43f19877471a29a38250c0df17153a51
muez_vx <bc180402947@vu.edu.pk>
2024-02-28 23:15:41 +0500

    Dashboard Content

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -145,68 +145,9 @@
          	</aside>
 			<!-- Content Wrapper. Contains page content -->
 			<div class="content-wrapper">
-				<!-- Content Header (Page header) -->
-				<section class="content-header">					
-					<div class="container-fluid">
-						<div class="row mb-2">
-							<div class="col-sm-6">
-								<h1>Dashboard</h1>
-							</div>
-							<div class="col-sm-6">
-								
-							</div>
-						</div>
-					</div>
-					<!-- /.container-fluid -->
-				</section>
-				<!-- Main content -->
-				<section class="content">
-					<!-- Default box -->
-					<div class="container-fluid">
-						<div class="row">
-							<div class="col-lg-4 col-6">							
-								<div class="small-box card">
-									<div class="inner">
-										<h3>150</h3>
-										<p>Total Orders</p>
-									</div>
-									<div class="icon">
-										<i class="ion ion-bag"></i>
-									</div>
-									<a href="#" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
-								</div>
-							</div>
-							
-							<div class="col-lg-4 col-6">							
-								<div class="small-box card">
-									<div class="inner">
-										<h3>50</h3>
-										<p>Total Customers</p>
-									</div>
-									<div class="icon">
-										<i class="ion ion-stats-bars"></i>
-									</div>
-									<a href="#" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
-								</div>
-							</div>
-							
-							<div class="col-lg-4 col-6">							
-								<div class="small-box card">
-									<div class="inner">
-										<h3>$1000</h3>
-										<p>Total Sale</p>
-									</div>
-									<div class="icon">
-										<i class="ion ion-person-add"></i>
-									</div>
-									<a href="javascript:void(0);" class="small-box-footer">&nbsp;</a>
-								</div>
-							</div>
-						</div>
-					</div>					
-					<!-- /.card -->
-				</section>
-				<!-- /.content -->
+				
+				@yield('content')
+
 			</div>
 			<!-- /.content-wrapper -->
 			<footer class="main-footer">
@@ -223,5 +164,7 @@
 		<script src="{{ asset('admin_assets/js/adminlte.min.js') }}"></script>
 		<!-- AdminLTE for demo purposes -->
 		<script src="{{ asset('admin_assets/js/demo.js') }}"></script>
+
+		@yield('customJs')
 	</body>
 </html>
